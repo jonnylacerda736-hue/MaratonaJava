97bd975581c347a420370b98d3d3f66b27a5dc44
Jonny Lacerda <jonnylacerda736@gmail.com>
2025-11-11 13:03:02 -0300

    como printar texto

--- Aula01.java
+++ Aula01.java
@@ -1,3 +1,6 @@
 public class Aula01 {
-    
+    public static void main(String[] args) {
+       System.out.println("------------------------");
+        System.out.println("sejá bem indo!!");
+    }
 }
