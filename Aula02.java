25c71eb234e4e868cf68cd2393feaabaeabe09f1
Jonny Lacerda <jonnylacerda736@gmail.com>
2025-11-12 00:08:07 -0300

    Tipos de Comentários

---
+++ Aula02.java
@@ -0,0 +1,13 @@
+public class Aula02 {
+    public static void main(String[] args) {
+        //isso é um comentário de uma linha
+
+        /*Comentário
+        de
+        multiplas
+        linhas
+         */
+
+        
+    }
+}
