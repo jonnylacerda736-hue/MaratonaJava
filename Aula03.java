9e0ab07cc23f7e71e4ed68b934427a1c7872b2a0
Jonny Lacerda <jonnylacerda736@gmail.com>
2025-11-12 00:29:16 -0300

    como declarar uma variavel e tipos..

--- Aula03.java
+++ Aula03.java
@@ -1,3 +1,10 @@
 public class Aula03 {
-    
+
+    public static void main(String[] args) {
+        // int,double, float, char, byte, short, long, booleam
+
+        int num = 10;
+
+        System.out.println(num);
+    }
 }
