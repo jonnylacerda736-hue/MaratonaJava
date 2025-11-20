5163c69eca122f17b7f8b47845e11c7dd8bbff3d
Jonny Lacerda <jonnylacerda736@gmail.com>
2025-11-20 14:50:44 -0300

    printando valores das variaveis

---
+++ ValoresAsVariaveis.java
@@ -0,0 +1,21 @@
+public class ValoresAsVariaveis {
+    public static void main(String[] args) {
+    int idade = 16;
+    double salarioDouble = 2000;
+    float salarioFloat = 2500;  //Armazena números decimais.
+    char caractere = 'J'; //caractere unica
+    byte idadeByte = 100; //números inteiros pequenos.
+    short idadeshort = 10; //rmazena inteiros maiores que byte e menores que int.
+    long numerogrande = 1000000; //inteiro grande
+    boolean verdadeiro = true; //verdadeiro ou falso
+
+    System.out.println("A idade de Jonny é :" + idade);
+    System.out.println("O salario double dele é: " + salarioDouble);
+    System.out.println("O salario float dele é: " + salarioFloat);
+    System.out.println("A letra dele fav é: " + caractere);
+    System.out.println("o numero da sorte dele é: " + idadeByte);
+    System.out.println("Ele gosto desse numero grande: " + idadeshort);
+    System.out.println("Esse numero é muito grande: " + numerogrande);
+    System.out.println("Ele gosta de pessoas: " + verdadeiro);
+    }
+}
