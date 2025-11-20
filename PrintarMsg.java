eb884227ec4208129df6cf6d8f3986bf5b88690e
Jonny Lacerda <jonnylacerda736@gmail.com>
2025-11-20 14:24:21 -0300

    declarando variaveis

--- PrintarMsg.java
+++ PrintarMsg.java
@@ -2,6 +2,16 @@ public class PrintarMsg {
     //Isso é um comentário de uma linha
     /* Isso é um comentário de multiplas linhas */
 
+
+    int a; //numeros inteiros
+    double b; //Armazena números decimais (ponto flutuante).
+    float c;  //Armazena números decimais.
+    char d; //caractere unica
+    byte e; //números inteiros pequenos.
+    short f; //rmazena inteiros maiores que byte e menores que int.
+    long g; //inteiro grande
+    boolean h; //verdadeiro ou falso
+
     public static void main(String[] args) {
         System.out.println("Como printar na tela");
     }
